834ec2c14839029dd7eb6db71fe7b66a995bc339
ZThijs1 <37913694+ZThijs1@users.noreply.github.com>
2019-10-04 11:31:42 +0200

    Some progress on layout

--- myapp.py
+++ myapp.py
@@ -3,12 +3,6 @@ from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtGui as qtg
 from PyQt5 import QtCore as qtc
 
-# from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-# from PyQt5.QtCore import Qt
-
-
-
-
 
 class MainWindow(qtw.QMainWindow):
 
@@ -23,15 +17,38 @@ class MainWindow(qtw.QMainWindow):
         ## Main UI code goes here
         self.label = qtw.QLabel(self)
         self.label.setText("This is a PyQt5 App")
-        self.label.move(50,50)
 
         self.button1 = qtw.QPushButton(self)
         self.button1.setText("Click Me")
-        self.button1.clicked.connect(self.clicked)
-
-        ## End main UI code
+        self.button1.clicked.connect(self.clickme)
         
-    def clicked(self):
+        self.button2 = qtw.QPushButton(self)
+        self.button2.setText("Open File")
+        self.button2.clicked.connect(self.open_file)
+
+        self.listbox = qtw.QListWidget(self)
+        self.listbox.setGeometry(0,0,100,200)
+        self.createLayout()
+        ## End main UI code
+
+    def createLayout(self):
+        layout1 = qtw.QGridLayout()
+        layout1.addWidget(self.label,0,0)
+        layout1.addWidget(self.listbox,1,1)
+        layout1.addWidget(self.button1,1,0)
+        layout1.addWidget(self.button2,2,0)
+        
+        widget = qtw.QWidget()
+        widget.setLayout(layout1)
+        self.setCentralWidget(widget)
+
+    def open_file(self):
+        filename = qtw.QFileDialog.getOpenFileName(self, 'Open File', './', "PDF Files (*.pdf)")
+        
+        if filename[0]:
+            print(filename[0])
+
+    def clickme(self):
         self.label.setText("Button pressed")
 
 if __name__ == '__main__':
